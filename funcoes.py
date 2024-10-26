41960d1bb93bc5594cb4ebffcf45882854d28f32
EnricoVBerto <enricovberto@hotmail.com>
2024-10-26 19:03:38 -0300

    ex_1

--- funcoes.py
+++ funcoes.py
@@ -1,1 +1,10 @@
-# TESTE
+def define_posicoes(linha,coluna,orientacao,tamanho):
+    posicao=[]
+    i=0
+    while i!=tamanho:
+        if orientacao=='vertical':
+            posicao.append([linha+i,coluna])
+        if orientacao=='horizontal':
+            posicao.append([linha,coluna+i])
+        i+=1
+    return posicao
