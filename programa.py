a95c9c18c7ef434a81a595f2354866019990e341
EnricoVBerto <enricovberto@hotmail.com>
2024-10-27 20:07:55 -0300

    colocando quantidade nas listas, arrumando o for

--- programa.py
+++ programa.py
@@ -5,13 +5,18 @@ frota = { "porta-aviões": [],
     "contratorpedeiro": [],
     "submarino": [] }
 
-lista_navios = [("porta-aviões", 4), ("navio-tanque", 3), ("contratorpedeiro", 2), ("submarino", 1)]
+lista_navios = [("porta-aviões", 4, 1), ("navio-tanque", 3, 2), ("contratorpedeiro", 2, 3), ("submarino", 1, 4)]
 
-for nome, tamanho in lista_navios:
-    while True:
-        linha = int(input("Digite a linha desejada: "))
-        coluna = int(input("Digite a coluna desejada: "))
-        if nome == "submarino":
-            orientacao = None
-        else:
-            orientacao = 'vertical'
+for nome, tamanho, quantidade in lista_navios:
+    for i in range(quantidade):
+        while True:
+            linha = int(input("Digite a linha desejada: "))
+            coluna = int(input("Digite a coluna desejada: "))
+            if nome == "submarino":
+                orientacao = None
+            else:
+                orientacao1=int(input('Qual orientação deseja?')) 
+                if orientacao1==1:
+                    orientacao='vertical'
+                if orientacao==2:
+                    orientacao='horizontal'
