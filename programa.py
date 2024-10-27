8a9224eb91d47e458a8976eac7ae8e55dfe3e1b9
João Pedro Hugues <joaopmh@al.insper.edu.br>
2024-10-27 20:33:42 -0300

    consertando o import de funções

--- programa.py
+++ programa.py
@@ -1,4 +1,4 @@
-from funcoes import *
+from funcoes import define_posicoes, preenche_frota, faz_jogada, posiciona_frota, afundados, posicao_valida
 
 frota = { "porta-aviões": [],
     "navio-tanque": [],
